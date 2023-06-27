5a9f8c8c00fcec7d034f97b325577228b5b6a191
dieseliz <55350191+dieseliz@users.noreply.github.com>
2023-06-27 15:21:14 +0200

    Add files via upload

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Lisa Meyer on 28.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
